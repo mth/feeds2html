690408df3441e4d19f4b6c96594d58fa952e93d7
Madis Janson <madis@cyber.ee>
2014-02-18 01:36:42 +0200

    read config

--- rss2html.hs
+++ rss2html.hs
@@ -3,9 +3,10 @@ import Control.Exception as E
 import Data.ByteString.UTF8 (toString)
 import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.Lazy.Char8 as CL
+import Data.Char
 import Data.Digest.Pure.SHA
 import Network.HTTP.Date
-import System.Environment
+-- import System.Environment
 import System.Exit
 import System.IO
 import System.Posix.Directory
@@ -15,8 +16,44 @@ import System.Process
 import Text.Feed.Import
 import Text.Feed.Query
 
+data FeedOption = PreserveOrder deriving Read
+data HtmlDef = H C.ByteString | Title deriving Read
+
+data ConfigItem =
+    Feed C.ByteString [FeedOption] |
+    Item [HtmlDef] |
+    Page [HtmlDef]
+    deriving Read
+
+data Config = Config { feeds :: [(String, [FeedOption])],
+                       items :: [[HtmlDef]],
+                       page  :: [HtmlDef] }
+
 tryIO :: IO a -> IO (Either E.IOException a)
 tryIO = E.try
+
+parseConfigItems str = skip parse 1 str
+  where skip _ line ('\n':s) = skip parse (line + 1) s
+        skip tr line (c:s) | isSpace c = skip tr line s
+        skip _ _ "" = Config { feeds = [], items = [], page = [] }
+        skip tr line str = tr line str
+        parse line str = case reads str of
+            ((result, tail):_) ->
+                let len = length str - length tail in
+                let !nl = line + length (filter (== '\n') (take len str)) in
+                compose result (skip err nl tail)
+            _ -> error (show line ++ ": syntax error")
+        err line _ = error (show line ++ ": expected newline after definition")
+        compose item cfg = case item of
+            Feed url opt -> cfg { feeds = (C.unpack url, opt) : feeds cfg }
+            Item html    -> cfg { items = html : items cfg }
+            Page html    -> cfg { page  = html ++ page cfg }
+
+readConfig cfgfile = parse `fmap` C.readFile cfgfile
+  where parse = parseConfigItems . C.unpack . C.unlines .
+                filter notComment . C.lines
+        notComment s = let s' = C.dropWhile isSpace s in
+                       C.null s' || C.head s' /= '#'
 
 runFork action = do
     result <- newEmptyMVar
@@ -70,5 +107,5 @@ fetchFeeds urls =
     mapM (runFork . fetchFeed) urls >>= mapM readMVar
 
 main = do
-    args <- getArgs
-    fetchFeeds args >>= mapM print
+    cfg <- readConfig ".rss2htmlrc"
+    fetchFeeds (map fst (feeds cfg)) >>= mapM print
